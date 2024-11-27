11cc1bdd5174e0c4b7c27b8bce78456b909dad81
mehdisafaee <mehdi.safaie7@gmail.com>
2024-11-27 18:02:33 +0330

    edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import ProtectedRoute from "./ui/ProtectedRoute";
-import { DarkModeProvider } from "./context/darkModeContext";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
